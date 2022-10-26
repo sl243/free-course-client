d0e674aeb14724e12444072d9bbff70a4fceb0ce
sl243 <slmiucse@gmail.com>
2022-10-26 15:31:53 +0600

    course details added in the course details page

--- src/components/Courses/CourseDetails/CourseDetails.js
+++ src/components/Courses/CourseDetails/CourseDetails.js
@@ -1,15 +1,38 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const CourseDetails = () => {
     const courseCategory = useLoaderData();
     console.log(courseCategory)
     return (
         <div>
-            <h3>Course Details {courseCategory.length}</h3>
-            <h3>{courseCategory[0].title}</h3>
-            <h3>{courseCategory[0].name}</h3>
-            <h3>{courseCategory[0].duration}</h3>
+            <Card className=" container mt-5 mb-5">
+                <div className='d-flex justify-content-around align-items-center mt-3'>
+                    <div>
+                        <Card.Header><h2>{courseCategory[0].name}</h2></Card.Header>
+                    </div>
+                    <div>
+                        <Button variant="primary">Download</Button>
+                    </div>
+                </div>
+                <Card.Body className='mt-3 mb-3'>
+                    <Card.Title className='mb-3 fs-3'>{courseCategory[0].title}</Card.Title>
+                    <Card.Img variant="top" style={{ height: '400px' }} src={courseCategory[0].image_url} />
+                    <Card.Text className='mt-4 fs-5'>
+                        {courseCategory[0].details}
+                    </Card.Text>
+                    <div className='d-flex justify-content-around align-items-center mt-3'>
+                        <div>
+                            <Card.Text><h4>Course Duration: {courseCategory[0].duration}</h4></Card.Text>
+                        </div>
+                        <div>
+                            <Button variant="primary">Get Enroll</Button>
+                        </div>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/components/Courses/Courses/Courses.js
+++ src/components/Courses/Courses/Courses.js
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Courses.css'
-import {useLoaderData } from 'react-router-dom';
-import CourseCategory from '../CourseCategory/CourseCategory';
-import CourseItems from '../CourseItems/CourseItems';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
 
 const Courses = () => {
-    const courses = useLoaderData();
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5000/course-categories')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
             <div className='container mt-5'>
                 <div className='row'>
-                    <div className='col-3'>
-                        <CourseCategory></CourseCategory>
+                    <div className='col-3 mt-5'>
+                        <h3 className='mb-3'>Course Categories</h3>
+                        {
+                            categories.map(category => <p key={category.id}>
+                                <Link to={`/courseDetails/${category.id}`} >
+                                    <Button className='fs-6' variant="outline-success">{category.name}</Button>
+                                </Link>
+                            </p>)
+                        }
                     </div>
                     <div className='col-9 course-container'>
                         {
-                            courses.map(course => <CourseItems
-                                key={course.id}
-                                course={course}
-                            ></CourseItems>)
+                            categories.map(card => <Card className="text-center container mt-5">
+                                <Card.Header className='mt-3'>
+                                    <h4>
+                                        {card.name}
+                                    </h4>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Text>
+                                        <Link to={`/courseDetails/${card.id}`} >
+                                            <Button className='fs-6' variant="outline-success">Details</Button>
+                                        </Link>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>)
                         }
                     </div>
                 </div>
